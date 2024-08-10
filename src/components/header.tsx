0fd7c34e1547b80697798dd01b498f6e0acf4017
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-08-10 18:07:50 -0400

    create header

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className='header'>
+      <h1>
+        Ghana<span>G</span>
+        <span>P</span>
+        <span>T</span>
+      </h1>
+    </header>
+  );
+}
