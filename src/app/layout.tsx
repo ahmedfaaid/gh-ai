ebaba05eda170345a201469f48edb72d927f78fc
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-08-11 00:03:51 -0400

    add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Noto_Sans } from 'next/font/google';
+import Head from 'next/head';
 import './globals.css';
 
 const noto = Noto_Sans({
@@ -18,6 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <Head>
+        <link rel='icon' href='/favicon.ico' sizes='any' />
+        <link
+          rel='icon'
+          href='/favicon/favicon-32x32.png'
+          type='image/png'
+          sizes='32x32'
+        />
+        <link
+          rel='apple-touch-icon'
+          href='/favicon/apple-icon.png'
+          type='image/png'
+          sizes='180x180'
+        />
+      </Head>
       <body className={noto.className}>{children}</body>
     </html>
   );
